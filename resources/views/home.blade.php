35c0304968241411afea390d1d4dd323aee09a6c
Dolon Roy <dolonr718@gmail.com>
2024-08-27 09:27:51 +0600

    dynamic route view learned commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid enim repellat nam repellendus nesciunt at odio ut
+    impedit unde laborum, soluta blanditiis, sint ab voluptas sequi recusandae, asperiores veniam vero.
+</p>
+<h1>
+    The name of the user is : {{$name}}
+</h1>
