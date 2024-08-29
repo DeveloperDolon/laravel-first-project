5e7377bc0616f457ff3cf0a7e1f0ebb3dc4291eb
Dolon Roy <dolonr718@gmail.com>
2024-08-29 11:35:25 +0600

    custom validation message learningn commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,54 +11,67 @@ class UserController extends Controller
         return "Code step by step.";
     }
 
-    function getUserName($name) {
+    function getUserName($name)
+    {
         return view("getUserName", ["name" => $name]);
     }
 
-    function getDetails($product) {
+    function getDetails($product)
+    {
         $products = [
             [
                 "name" => "Monitor 1D G5",
                 "brand" => "Samsung",
                 "price" => 34000,
-                "description" => "This is a 1D G5 monitor." 
+                "description" => "This is a 1D G5 monitor."
             ],
             [
                 "name" => "Samurai 545",
                 "brand" => "Samsung",
                 "price" => 34600,
-                "description" => "This is a Samurai 545 monitor." 
+                "description" => "This is a Samurai 545 monitor."
             ],
             [
                 "name" => "Ultra gaming monitor",
                 "brand" => "Samsung",
                 "price" => 234000,
-                "description" => "This is a Ultra gaming monitor." 
+                "description" => "This is a Ultra gaming monitor."
             ],
             [
                 "name" => "Monitor 1D G5",
                 "brand" => "Samsung",
                 "price" => 34000,
-                "description" => "This is a 1D G5 monitor." 
+                "description" => "This is a 1D G5 monitor."
             ]
         ];
         return view("productDetails", ["product" => $product, "products" => $products]);
     }
 
-    function addUser(Request $req) {
-
-        $req->validate([
-            "name" => "required | min:3 | max:10",
-            "email" => "required | email",
-            "password" => "required | password",
-            "phone_number" => "required | digits:11"
-        ]);
+    function addUser(Request $req)
+    {
+        $req->validate(
+            [
+                "name" => "required | min:3 | max:10",
+                "email" => "required | email",
+                "password" => "required | password",
+                "mobile_number" => "required | digits:11",
+                "skill" => "required"
+            ],
+            [
+                "name.required" => "The attribute field is required.",
+                "name.min" => "The attribute field must be at least :min characters.",
+                "name.max" => "The attribute field must be less than or equal to :max characters.",
+                "email.email" => "The attribute field must be a valid email address.",
+                "password.password" => "The attribute field must be a valid password.",
+                "skill.required" => "The attribute field must"
+            ]
+        );
 
         echo "User name is $req->name";
         echo "<br>";
         echo "User email is $req->email";
         echo "<br>";
-        echo "User phone number is $req->phone_number";
+        echo "User phone number is $req->mobile_number";
         echo "<br>";
         echo "User password is $req->password";
     }
--- resources/views/user-form.blade.php
+++ resources/views/user-form.blade.php
@@ -1,30 +1,73 @@
 <div>
     <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-    <form method="POST" action="adduser">
+    <form class="user-info-form" method="POST" action="adduser">
         @csrf
-        <input value="{{old('name')}}" placeholder="Input your name" class="input-field" type="text" name="name" id="">
-        <br>
-        <input value="{{old('email')}}" placeholder="Input your email" class="input-field" type="email" name="email" id="">
-        <br>
-        <input value="{{old('mobile_number')}}" placeholder="Input your mobile number" class="input-field" type="text" name="mobile_number"
+        <input value="{{ old('name') }}" placeholder="Input your name" class="input-field" type="text" name="name"
             id="">
         <br>
-        <input value="{{old('passworld')}}" placeholder="Input you password" class="input-field" type="text" name="password" id="">
+        <span style="color: red; margin: 5px 0px;">
+            @error('name')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        <button type="submit">Submit your information</button>
+        <input value="{{ old('email') }}" placeholder="Input your email" class="input-field" type="email"
+            name="email" id="">
+        <br>
+        <span style="color: red; margin: 5px 0px;">
+            @error('email')
+                {{ $message }}
+            @enderror
+        </span>
+        <br>
+        <input value="{{ old('mobile_number') }}" placeholder="Input your mobile number" class="input-field"
+            type="text" name="mobile_number" id="">
+        <br>
+        <span style="color: red; margin: 5px 0px;">
+            @error('mobile_number')
+                {{ $message }}
+            @enderror
+        </span>
+        <br>
+        <input value="{{ old('password') }}" placeholder="Input you password" class="input-field" type="text"
+            name="password" id="">
+        <br>
+        <label for="java">
+            Java
+            <input type="checkbox" name="java" id="skill" value="Java">
+        </label>
+        <label for="python">
+            Python
+            <input type="checkbox" name="python" id="skill" value="Python">
+        </label>
+        <label for="PHP">
+            PHP
+            <input type="checkbox" name="php" id="skill" value="PHP">
+        </label>
+        <br>
+        <span style="color: red; margin: 5px 0px;">
+            @error('skill')
+                {{ $message }}
+            @enderror
+        </span>
+        <br>
+        <br>
+        <button type="submit">Submit Info</button>
     </form>
-
-    {{-- @if ($user_info)
-        <h1>User information {{$user_info}}</h1>
-    @endif --}}
 </div>
 
 <style>
+
+
     .input-field {
         padding: 6px 10px;
         border-radius: 5px;
         background: rgb(230, 230, 230);
-        margin-bottom: 10px;
         border: none;
+    }
+
+    .user-info-form span {
+        display: inline-block;
+        padding: 5px 0px 10px;
     }
 </style>
