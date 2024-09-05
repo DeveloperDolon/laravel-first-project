b321c6b7ccb5c04de1c19835d46803da38c32539
Dolon Roy <dolonr718@gmail.com>
2024-09-05 16:38:16 +0600

    test api created and run complete commit

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get("/hello_world", function() 
+{
+    return ["name" => "Dolon Chandra", "age" => 22, "profession" => "Software Engineer"];
+});
